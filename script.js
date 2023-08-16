d1c3df9601d7834b6db1ac0cfdeddb2f0a4ee24c
Bilaal Latif <bilaallatif@outlook.com>
2023-08-17 00:15:05 +0100

    Added clear and slider functionality with styling

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 let grid = document.querySelector('#grid');
-const GRID_WIDTH = 10;
-for (let i = 0; i < GRID_WIDTH ** 2; i++) {
-    let cell = document.createElement('div');
-    cell.classList.add('cell');
-    cell.style.flexBasis = `${100 / GRID_WIDTH}%`;
-    cell.addEventListener('mouseover', () => {cell.style.backgroundColor = 'white';});
-    grid.appendChild(cell);
+let grid_size = document.querySelector('#grid-size');
+let slider_text = document.querySelector('#slider-text');
+let clear_button = document.querySelector('#clear');
+
+function render_grid(grid_width) {
+    grid.innerHTML = '';
+    
+    for (let i = 0; i < grid_width ** 2; i++) {
+        let cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.style.flexBasis = `${100 / grid_width}%`;
+        cell.addEventListener('mouseover', () => {cell.style.backgroundColor = 'white';});
+        grid.appendChild(cell);
+    }
 }
+
+render_grid(grid_size.value);
+
+grid_size.addEventListener('change', () => {
+    slider_text.textContent = `${grid_size.value} x ${grid_size.value}`;
+    render_grid(grid_size.value);
+});
+
+clear_button.addEventListener('click', () => {render_grid(grid_size.value);});
+
+
+
+
